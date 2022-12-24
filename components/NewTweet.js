d4b9ced5f39409e11a5bbf04d7b3a060c924406e
lyjeileen <lyjeileen@gmail.com>
2022-12-24 00:09:11 -0800

    Add text box

--- components/NewTweet.js
+++ components/NewTweet.js
@@ -1,4 +1,6 @@
 import { useSession } from 'next-auth/react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 export default function NewTweet() {
   const { data: session } = useSession();
@@ -6,5 +8,35 @@ export default function NewTweet() {
   //don't display if we're not logged in
   if (!session) return null;
 
-  return <p>form placeholder!</p>;
+  return (
+    <>
+      <Box
+        component="form"
+        //add padding
+        sx={{
+          '& .MuiTextField-root': { p: 3 },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          fullWidth
+          id="outlined-multiline-flexible"
+          label="What are you humming about?"
+          multiline
+          maxRows={4}
+          //add margin becuase of padding on Box
+          sx={{
+            '& label': {
+              marginLeft: 3,
+              marginTop: 3,
+              '&.Mui-focused': {
+                marginLeft: 3,
+              },
+            },
+          }}
+        />
+      </Box>
+    </>
+  );
 }
