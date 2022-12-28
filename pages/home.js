afba5ca0d2e062b56e2b8df4e032e64aa2c97e4e
lyjeileen <lyjeileen@gmail.com>
2022-12-27 23:23:05 -0800

    Add Tweets and getServerSideProps

--- pages/home.js
+++ pages/home.js
@@ -1,16 +1,40 @@
 import { useSession } from 'next-auth/react';
-import NewTweet from 'components/NewTweet';
+import { useRouter } from 'next/router';
+import prisma from 'lib/prisma';
+import { getTweets } from 'lib/data';
 
-export default function Home() {
+import NewTweet from 'components/NewTweet';
+import Tweets from 'components/Tweets';
+
+export default function Home({ tweets }) {
   const { data: session, status } = useSession();
+  const router = useRouter();
 
   if (status === 'loading') {
-    return <p>...</p>;
+    return null;
+  }
+
+  if (!session) {
+    router.push('/');
   }
 
   return (
     <div className="p-5">
-      {session ? <NewTweet /> : <p>You are not logged in 😞</p>}
+      <NewTweet />
+      <Tweets tweets={tweets} />
     </div>
   );
 }
+
+export const getServerSideProps = async () => {
+  let tweets = await getTweets(prisma);
+  //convert createdAt timestamp to string format(getServerSideProps returns data as JSON-encoded, JSON doesn't support Date objects)
+  tweets = JSON.parse(JSON.stringify(tweets));
+
+  return {
+    //pass tweets to Home as props
+    props: {
+      tweets,
+    },
+  };
+};
