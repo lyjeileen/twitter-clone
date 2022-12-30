c7b86109f9dce9a325f542665fc109dff7745936
lyjeileen <lyjeileen@gmail.com>
2022-12-30 00:22:22 -0800

    Add formated createdAt to tweet

--- components/Tweet.js
+++ components/Tweet.js
@@ -1,3 +1,10 @@
+import timeago from 'lib/timeago';
+
 export default function Tweet({ tweet }) {
-  return <p>{tweet.content}</p>;
+  return (
+    <>
+      <p>{tweet.content}</p>
+      <p>{timeago.format(new Date(tweet.createdAt))}</p>
+    </>
+  );
 }
