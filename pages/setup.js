768b0af8415c3ca40dc60689820bcf67a3e52bb4
lyjeileen <lyjeileen@gmail.com>
2023-01-05 00:24:21 -0800

    Add error message for POST request

--- pages/setup.js
+++ pages/setup.js
@@ -7,10 +7,12 @@ import TextField from '@mui/material/TextField';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import InputAdornment from '@mui/material/InputAdornment';
 import Button from '@mui/material/Button';
+import Alert from '@mui/material/Alert';
 
 export default function Setup() {
   const { data: session, status } = useSession();
-  const [name, setName] = useState('');
+  const [username, setUsername] = useState('');
+  const [error, setError] = useState();
   const router = useRouter();
 
   if (!session || !session.user) return null;
@@ -27,19 +29,34 @@ export default function Setup() {
       className="m-auto flex flex-col w-fit"
       onSubmit={async (e) => {
         e.preventDefault();
-        await fetch('/api/setup', {
-          body: JSON.stringify({
-            name,
-          }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          method: 'POST',
-        });
-        session.user.name = name;
-        router.push('/home');
+
+        if (!username) {
+          setError('Username cannot be empty');
+        } else {
+          let response = await fetch('/api/setup', {
+            body: JSON.stringify({
+              username,
+            }),
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            method: 'POST',
+          });
+
+          if (response.status === 200) {
+            session.user.name = username;
+            router.push('/home');
+          } else {
+            const error = await response.json();
+            console.log(error);
+            if (error) {
+              setError(error.message);
+            }
+          }
+        }
       }}
     >
+      {error && <Alert severity="warning">{error}</Alert>}
       <p className="pt-3">Enter a username</p>
       <TextField
         id="input-with-icon-textfield"
@@ -52,8 +69,8 @@ export default function Setup() {
           ),
         }}
         variant="standard"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
       />
       <div>
         <Button type="submit" variant="contained" className="text-[#ec4899]">
