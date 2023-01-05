3cbeb6025e08dfad5d023bb9e729abbe899c9db1
lyjeileen <lyjeileen@gmail.com>
2023-01-05 00:25:05 -0800

    Add catch for error

--- pages/api/setup.js
+++ pages/api/setup.js
@@ -1,16 +1,25 @@
 import prisma from 'lib/prisma';
+import { Prisma } from '@prisma/client';
 import { getSession } from 'next-auth/react';
 
 export default async function handler(req, res) {
   const session = await getSession({ req });
+  console.log(session);
   if (!session) return res.end();
-
+  console.log('body', req.body);
   if (req.method === 'POST') {
-    await prisma.user.update({
-      where: { email: session.user.email },
-      data: { name: req.body.name },
-    });
-
-    res.end();
+    try {
+      await prisma.user.update({
+        where: { email: session.user.email },
+        data: { username: req.body.username },
+      });
+      res.end();
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        if (e.code === 'P2002') {
+          res.status(403).json({ message: 'Username already exists.' });
+        }
+      }
+    }
   }
 }
