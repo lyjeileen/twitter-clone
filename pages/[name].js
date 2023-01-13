4893f5facd987972ebff53bc4946345482eb869e
lyjeileen <lyjeileen@gmail.com>
2023-01-12 23:46:35 -0800

    Add conditional rendering for profile
    If user doesn't exist, an error will show instead

--- pages/[name].js
+++ pages/[name].js
@@ -6,15 +6,23 @@ import Tweets from 'components/Tweets';
 export default function UserProfile({ name, tweets }) {
   return (
     <>
-      <h2>User profile of {name}</h2>
-      <Tweets tweets={tweets} />
+      {tweets.length !== 0 ? (
+        <div>
+          <h2>User profile of {name}</h2>
+          <Tweets tweets={tweets} />
+        </div>
+      ) : (
+        <p>ERROR: Page not found</p>
+      )}
     </>
   );
 }
 
 export const getServerSideProps = async ({ params }) => {
   let userTweets = await getUserTweets(prisma, params.name);
-  userTweets = JSON.parse(JSON.stringify(userTweets));
+  if (userTweets.length !== 0) {
+    userTweets = JSON.parse(JSON.stringify(userTweets));
+  }
   return {
     props: {
       name: params.name,
