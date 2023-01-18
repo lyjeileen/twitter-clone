6078b81e3cb80056844eeb7c7db7ac9a3587c344
lyjeileen <lyjeileen@gmail.com>
2023-01-17 16:52:07 -0800

    Add Avatar component

--- components/Tweet.js
+++ components/Tweet.js
@@ -1,5 +1,6 @@
 import timeago from 'lib/timeago';
 //next/image has better performance than html image
+import { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useSession } from 'next-auth/react';
@@ -9,39 +10,18 @@ import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import ShareIcon from '@mui/icons-material/Share';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
+import NewComment from 'components/NewComment';
+import Avatar from 'components/Avatar';
+
 export default function Tweet({ tweet }) {
   const { data: session, status } = useSession();
   const router = useRouter();
-
+  const [newComment, setNewComment] = useState(false);
   return (
     <div className="flex border-solid p-2 my-8 border-2 border-red-400 rounded-lg hover:shadow-md">
       {/* use flex-shrink-0 to avoid the change of avatar size in small screen. flex in the outer box could shrink avatar */}
       <div className="mr-2 flex-shrink-0">
-        {tweet.author.image ? (
-          <Image
-            className="rounded-full ring-2 ring-red-400"
-            src={tweet.author.image}
-            alt="Author name"
-            width={40}
-            height={40}
-          />
-        ) : (
-          //show a placeholder icon for the user profile when there is no custom image available
-          <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full ring-2 ring-red-400 dark:bg-gray-600 ">
-            <svg
-              className="absolute w-12 h-12 text-gray-400 -left-1"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-          </div>
-        )}
+        <Avatar image={tweet.author.image} />
       </div>
 
       <div className="flex-auto">
@@ -84,11 +64,15 @@ export default function Tweet({ tweet }) {
           {session && session.user.email && (
             <div className="flex justify-evenly min-w-[88px] text-red-400">
               <FavoriteBorderIcon className="text-[16px] hover:text-red-800" />
-              <ChatBubbleOutlineIcon className="text-[16px] hover:text-red-800" />
+              <ChatBubbleOutlineIcon
+                onClick={() => setNewComment(!newComment)}
+                className="text-[16px] hover:text-red-800"
+              />
               <ShareIcon className="text-[16px] hover:text-red-800" />
             </div>
           )}
         </div>
+        {newComment && <NewComment tweet={tweet} />}
       </div>
     </div>
   );
